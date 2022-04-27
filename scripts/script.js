5bffab040e748afe4d99499f175722117de16e14
joão vinícius <j.vin7cius@gmail.com>
2022-04-27 18:16:56 -0300

    Rascunho Carousel

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+// Rascunho;
+let a = document.getElementsByClassName("internal-structure")[0];
+let b = document.getElementsByClassName("internal-structure")[1];
+let c = document.getElementsByClassName("internal-structure")[2];
+const back = document.querySelector(".back");
+const forward = document.querySelector(".forward");
+
+let n = 0;
+
+back.addEventListener("click", function () {
+    if (n == 0) {
+        n = -200
+    } else if (n == -100) {
+        n+=100;
+    } else if (n == -200) {
+        n+=100;
+    }
+    a.style.transform = "translateX(" + n + "%)";
+    b.style.transform = "translateX(" + n + "%)";
+    c.style.transform = "translateX(" + n + "%)";
+});
+
+forward.addEventListener("click", function () {
+    if (n == 0) {
+        n-=100;
+    } else if (n == -100) {
+        n-=100;
+    } else if (n == -200) {
+        n = 0;
+    }
+    console.log(n)
+    a.style.transform = "translateX(" + n + "%)";
+    b.style.transform = "translateX(" + n + "%)";
+    c.style.transform = "translateX(" + n + "%)";
+});
